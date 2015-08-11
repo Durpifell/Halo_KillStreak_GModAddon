9995e8d416cc924b0cd0bdcab353b17474dcf86e
Dylan Walseth <DylanWalseth@gmail.com>
2015-08-10 21:41:53 -0500

    Sleeptime, work later, databaseHandler probably not actually needed, don't care enough to remove, call back later.

--- lua/autorun/server/kill.lua
+++ lua/autorun/server/kill.lua
@@ -13,12 +13,11 @@ function Kill(killer, victim, weapon, time)
 		end
       	weapon = weapon:GetClass()
 
-      	PrintableData = function(self)
-			return self.killer + " (" + self.killerId ") killed " + self.victim + " (" + self.vitcimId + ") with " + self.weapon + " at " + self.time 
-		end
+      	PrintableData = killer + " (" + killerId ") killed " + victim + " (" + vitcimId + ") with " + weapon + " at " + time
+
 		TableName = "KillsTable"
-		QueryTableCreateStatement = "CREATE TABLE "..TableName.." (".." KillerName varchar(255), KillerId varchar(30), Victim varchar(255), VictimId varchar(255), Weapon varchar(255), Time Timestamp"
-		QueryInsertStatement = ""
+		QueryTableCreateStatement = "CREATE TABLE "..TableName.." ( KillerName varchar(255), KillerId varchar(30), Victim varchar(255), VictimId varchar(255), Weapon varchar(255), Time timestamp )"
+		QueryInsertStatement = "INSERT INTO "..TableName.." VALUES ( " table.concat({killer, killerId, vitcim, victimId, weapon, time}, ", ")
 	}
 	return killData
 end
